a0c37cc05e0ea73e57070184d0c1b22c6930bae8
Aniruddha Ravi <aniruddha.ravi@greychaindesign.com>
2024-10-16 19:41:51 +0530

    Update settings.py

--- gutenberg_api/settings.py
+++ gutenberg_api/settings.py
@@ -97,7 +97,7 @@ import dj_database_url
 
 # Use Heroku Postgres
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600)
 }
 
 
